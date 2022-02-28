698135081f66debc2d6e0fcb5b20f5b787363f3a
zyf2021 <79312743+zyf2021@users.noreply.github.com>
2022-02-28 10:13:49 +0300

    Изменено главное меню

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -1,33 +1,89 @@
-import * as React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Box from '@material-ui/core/Box'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/MenuOpenOutlined';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/icons-material/Circle';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const pages = ['Обучение', 'Практика', 'Работа'];
+const menu_study = ['Поиск преподавателей', 'Поиск курсов'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export function MainMenu() {
+
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const openMenu = Boolean(anchorElUser)
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <AppBar position="static" elevation = {0}> 
+      <Container maxWidth="xl">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Система обучения
-          </Typography>
+        <Box sx={{flexGrow: 1, display: 'flex', justifyContent:'space-between', width:'100%', aligh:'center'}} component = 'div'>
+          <Box sx={{display:'flex'}}>
+            <IconButton>
+              <MenuIcon sx={{color:'white'}} fontSize='large'/>
+            </IconButton>
+          </Box>
+
+          <Box sx={{display:'flex'}}>
+            <div style={{marginRight:'10px'}}>
+            <Typography 
+            aria-aria-controls='edu-menu'
+            aria-aria-haspopup='true'
+            aria-aria-expanded={openMenu ? 'true' : undefined}
+            onClick = {handleOpenUserMenu}
+            sx = {{cursor:'pointer', color:'inherit'}}>
+              ОБУЧЕНИЕ
+            </Typography>
+            <Menu id = 'edu-menu' anchorEl={anchorElUser} open={openMenu} onClose={handleCloseUserMenu} width='20px'>
+              <MenuItem onClick={handleCloseUserMenu}><Typography textAlign="center">Поиск</Typography></MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}><Typography textAlign="center">Поиск</Typography></MenuItem>
+            </Menu>
+            </div>
+
+            <div style={{marginRight:'10px'}}>
+            <Typography
+            sx = {{marginRight:'10px', cursor:'pointer', color:'inherit'}}>
+              ПРАКТИКА
+            </Typography>
+            </div>
           
+            <div style={{marginRight:'10px'}}>
+            <Typography
+            sx = {{marginRight:'10px', cursor:'pointer', color:'inherit'}}>
+              РАБОТА
+            </Typography>
+            </div>
+          </Box> 
+           
+          <Box sx={{display:'flex'}}>
+            <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar fontSize='large' sx ={{color:'white'}}/>
+                </IconButton>
+            </Tooltip>
+          </Box>
+        </Box>
         </Toolbar>
-      </AppBar>
-    </Box>
+      </Container>
+    </AppBar>
   )
 }
 
