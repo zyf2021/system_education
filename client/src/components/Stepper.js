127eae5e2019a9a6e9aeba44756374357e6fb151
zyf2021 <79312743+zyf2021@users.noreply.github.com>
2022-02-28 23:29:49 +0300

    Создан ки

---
+++ client/src/components/Stepper.js
@@ -0,0 +1,54 @@
+import { Carousel } from 'react-carousel-minimal';
+
+export function Stepper() {
+ const data = [
+    {
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+      caption: ""
+    },
+    {
+      image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+      caption: ""
+    },
+    {
+      image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
+      caption: ""
+    }
+  ];
+
+  const captionStyle = {
+    fontSize: '2em',
+    fontWeight: 'bold',
+  }
+  const slideNumberStyle = {
+    fontSize: '20px',
+    fontWeight: 'bold',
+  }
+  return (
+      <div style={{ textAlign: "center" }}>
+        <Carousel
+            data={data}
+            time={6000}
+            width="910px"
+            height="500px"
+            captionStyle={captionStyle}
+            radius="10px"
+            slideNumber={false}
+            captionPosition="bottom"
+            automatic={true}
+            dots={true}
+            pauseIconColor="white"
+            pauseIconSize="40px"
+            slideBackgroundColor="darkgrey"
+            slideImageFit="cover"
+            thumbnails={false}
+            style={{
+              textAlign: "center",
+              maxWidth: "910px",
+              maxHeight: "500px",
+              margin: "10px 0px 0px 0px",
+            }}
+          />
+      </div>
+  );
+}
